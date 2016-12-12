ff483b8109443ad0eeaa7b761f750d905b828051
Kerby Ferris <kerbyferris@gmail.com>
2016-12-11 16:26:12 -0800

    answer slack request challenge

--- app.py
+++ app.py
@@ -1,31 +1,20 @@
+import logging
 from chalice import Chalice
 
 app = Chalice(app_name='loopmoon-api')
+app.log.setLevel(logging.DEBUG)
+app.debug = True
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return {'hello': 'worldd'}
 
 
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
+@app.route('/event', methods=['POST'])
+def url_verification():
+    request = app.current_request
+    event = request.json_body
+
+    if event['type'] == 'url_verification':
+        return {'challenge': event['challenge']}
