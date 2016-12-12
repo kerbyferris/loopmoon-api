af37a6716b34894bc7d08539a555acedc52aa7f4
Kerby Ferris <kerbyferris@gmail.com>
2016-12-11 16:34:06 -0800

    refactor event handler name with comments

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def index():
 
 
 @app.route('/event', methods=['POST'])
-def url_verification():
+def event():
     request = app.current_request
     event = request.json_body
 
+    # Respond appropriate to Slack request URL challenge
     if event['type'] == 'url_verification':
         return {'challenge': event['challenge']}
